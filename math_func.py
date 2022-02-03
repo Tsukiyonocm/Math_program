6af65821449195f6959a7ab9e3505503ca966de9
Tsukiyonocm <tsukiyonocm@yahoo.com>
2022-02-03 10:47:34 -0500

    basic layout setup, labels update, inactive entry box

---
+++ math_func.py
@@ -0,0 +1,11 @@
+# Generate Random Numbers for Addition Problems
+import random
+
+def gen_ran_num():
+    global num_a
+    num_return = random.randint(0, 10)
+    print(num_return)
+    return num_return
+
+# def check_answer():
+    
